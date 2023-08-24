b65d66cecc36b4c459677f5577a4441206e2dd25
124417985+vaccinaldig3710@users.noreply.github.com <124417985+vaccinaldig3710@users.noreply.github.com>
2023-08-24 18:11:22 -0500

    Fixed an issue with updating players names, and added an event listener for the select menu

--- script.js
+++ script.js
@@ -1,219 +1,226 @@
-// This should santize the users input and prevent XSS attacks. This is for security reasons
-const sanitizeModule = (function(){
-    const sanitizeInput = (input) => {
-        // Creates an empty, invisible div
-            const div = document.createElement('div');
-            //sets the div to input value
-            div.textContent = input;
-            // returns only its innerHTML, which prevents any of it from adding to my html
-            return div.innerHTML;
-        }
-    return { sanitizeInput };
-})();
-// This will get dom elements one time
-const domElements = {
-    mainElement: document.getElementById('main')
-};
-// Get all helpers value here so I don't need to type querySelector as much
-const domHelpers = {
-    getRadioValue: function() {
-        return domElements.mainElement.querySelector('#radio').value;
-    },
-    player1NameInput:  function() {
-        return sanitizeModule.sanitizeInput(domElements.mainElement.querySelector('.get-player1-name-input').value);
-    },
-    player2NameInput: function(){
-        return sanitizeModule.sanitizeInput(domElements.mainElement.querySelector('.get-player2-name-input').value);
-    },
-    getForm: function() {
-        return domElements.mainElement.querySelector('#form-container');
-    }
-};
+document.addEventListener('DOMContentLoaded', () => {
 
-
-const validationModule = (function() {
-    const isValidInput = (input) => {
-        return input && input.length >= 1 && input.length <= 10 && /^[a-zA-Z0-9!@#$%^&*()_+,\-./:;<=>?@[\]^_`{|}~]+$/.test(input);
+    // This should santize the users input and prevent XSS attacks. This is for security reasons
+    const sanitizeModule = (function(){
+        const sanitizeInput = (input) => {
+            // Creates an empty, invisible div
+                const div = document.createElement('div');
+                //sets the div to input value
+                div.textContent = input;
+                // returns only its innerHTML, which prevents any of it from adding to my html
+                return div.innerHTML;
+            }
+        return { sanitizeInput };
+    })();
+    // This will get dom elements one time
+    const domElements = {
+        mainElement: document.getElementById('main')
     };
-    const validatePlayerName = (input) => {
-        if (isValidInput(input)) {
-            return true;
-        } else if (domHelpers.getRadioValue() !== 'player2') {
-            return true;
-        } else {
-            return false;
+    // Get all helpers value here so I don't need to type querySelector as much
+    const domHelpers = {
+        getRadioValue: function() {
+            return domElements.mainElement.querySelector('#radio').value;
+        },
+        player1NameInput:  function() {
+            return sanitizeModule.sanitizeInput(domElements.mainElement.querySelector('.get-player1-name-input').value);
+        },
+        player2NameInput: function(){
+            return sanitizeModule.sanitizeInput(domElements.mainElement.querySelector('.get-player2-name-input').value);
+        },
+        getForm: function() {
+            return domElements.mainElement.querySelector('#form-container');
         }
-    }
-    return { validatePlayerName } 
-})();
-
-
-
-
-// This will be a module that will contain the gameBoard
-const boardModule = (function() {
-    // Clear game board
-    const _clearBoard = function() {
-        const gridElements = domElements.mainElement.querySelectorAll('.grid-item');
-        gridElements.forEach(element => element.parentNode.removeChild(element));
     };
 
-    const createBoard = function() {
-        _clearBoard();           
-        const gameContainer = domElements.mainElement.querySelector('#game-container');
-        // Loop to create the rows
-        for (let i = 0; i < 9; i++) {
-            const gridItem = document.createElement('div');
-            gridItem.classList.add('grid-item');
-            gameContainer.appendChild(gridItem);
-            }
-        }       
-    // Return it so I can call it later
-    return {createBoard}
-})();
 
-// Function to show the form, and hide the form 
+    // This will be a module that will contain the gameBoard
+    const boardModule = (() => {
+        // Clear game board
+        const _clearBoard = () => {
+            const gridElements = domElements.mainElement.querySelectorAll('.grid-item');
+            gridElements.forEach(element => element.parentNode.removeChild(element));
+        };
 
-const formControl = (function() {
-    // Function to show the form for names
-    const showForm = () => {
-        let form = domHelpers.getForm();
-        form.classList.add('active');
-        form.style.opacity = 1;
-        if (domHelpers.getRadioValue() !== 'player2') {
-            form.querySelector('.get-player2-name-input').style.opacity = 0;
-        }
-    }
-    // Function to hide form
-    const hideForm = () => {
-        let form = domHelpers.getForm();
-        form.classList.remove('active');
-        form.style.opacity = 0;
-    }
-    return {
-        showForm: showForm,
-        hideForm: hideForm
-    }
-})();
+        const createBoard = function() {
+            _clearBoard();           
+            const gameContainer = domElements.mainElement.querySelector('#game-container');
+            // Loop to create the rows
+            for (let i = 0; i < 9; i++) {
+                const gridItem = document.createElement('div');
+                gridItem.classList.add('grid-item');
+                gameContainer.appendChild(gridItem);
+                }
+            }       
+        // Return it so I can call it later
+        return {createBoard}
+    })();
 
-// Event listener to show button
-const listenerModule = (() => {
-    const listener = () => {
-        const newGameBtn = domElements.mainElement.querySelector('#new-game-btn');
-        newGameBtn.addEventListener('click', formControl.showForm);
-    }
-     return { listener }; 
-})();
-    
+    // Function to show the form, and hide the form 
 
-
-// Creates players that can share characteristics
-
-const createPlayer = (name, symbol, score) => {
-    return {
-        name,
-        symbol,
-        score,
-        makeMove: function(e) {
-            if (e.target.textContent !== '') return;
-            e.target.textContent = this.symbol;
-            // Determine current Player
-            let currentPlayer = player1;
-        }
-    }
-}
-
-
-const elementsModule = (function() {
-    function createElement(tag, attribute = {}, children = []) {
-        const element = document.createElement(tag);
-        for (let key in attribute) {
-            if (key === "textContent") {
-                element.textContent = attribute[key];
-            } else {
-                element.setAttribute(key, attribute[key]);
+    const formControl = (function() {
+        // Function to show the form for names
+        const showForm = () => {
+            let form = domHelpers.getForm();
+            form.classList.add('active');
+            form.style.opacity = 1;
+            if (domHelpers.getRadioValue() !== 'player2') {
+                form.querySelector('.get-player2-name-input').style.opacity = 0;
             }
         }
-        children.forEach(child => {
-            if (typeof child === 'string') {
-                element.appendChild(document.createTextNode(child));
-            } else {
-                element.appendChild(child);
+        // Function to hide form
+        const hideForm = () => {
+            let form = domHelpers.getForm();
+            form.classList.remove('active');
+            form.style.opacity = 0;
+        }
+        return {
+            showForm: showForm,
+            hideForm: hideForm
+        }
+    })();
+
+       
+
+
+    // Creates players that can share characteristics
+
+    const createPlayer = (name, symbol, score) => {
+        return {
+            name,
+            symbol,
+            score,
+            makeMove: function(e) {
+                if (e.target.textContent !== '') return;
+                e.target.textContent = this.symbol;
+                // Determine current Player
+                let currentPlayer = player1;
             }
-        });
-        return element;
+        }
     }
-    return {
-        createElement: createElement
-    }
-})();
 
 
-
-// Creates players displays, and shows their names and score
-const renderModule = (function() {
-    const player1Name = domElements.mainElement.querySelector('#player1-name');
-    const player2Name = domElements.mainElement.querySelector('#player2-ai-name');
-    const inputPlayer1Placeholder = domElements.mainElement.querySelector('.get-player1-name-input').getAttribute('placeholder');
-    const inputPlayer2Placeholder = domElements.mainElement.querySelector('.get-player1-name-input').getAttribute('placeholder')
-
-
-    const renderPlayerInfo = () => {
-        const renderPlayer1 = (() => {
-            if (validationModule.validatePlayerName(domHelpers.player1NameInput) === false) {
-                inputPlayer1Placeholder.textContent = "REQUIRED"
-            } else if (validationModule.validatePlayerName(domHelpers.player1NameInput) === true) {
-                player1Name.textContent = domHelpers.player1NameInput;
-            } 
-        })();
-        const renderPlayer2 = (() => {
-            if (validationModule.validatePlayerName(domHelpers.player2NameInput) === false) {
-                inputPlayer2Placeholder.textContent = "REQUIRED"
-            } else if (validationModule.validatePlayerName(domHelpers.player2NameInput) === true) {
-                if (domHelpers.getRadioValue === 'player2') {
-                    player2Name.textContent === domHelpers.player2NameInput;
+    const elementsModule = (function() {
+        function createElement(tag, attribute = {}, children = []) {
+            const element = document.createElement(tag);
+            for (let key in attribute) {
+                if (key === "textContent") {
+                    element.textContent = attribute[key];
                 } else {
-                    let aiName = '';
-                    switch (domHelpers.getRadioValue()) {
-                        case 'easy': aiName = "Locke"; break;
-                        case 'normal': aiName = "Tom"; break;
-                        case 'hard': aiName = "John"; break;
-                        default: aiName = domHelpers.player2NameInput; break;
-                    }
-                    player2Name.textContent = aiName;
+                    element.setAttribute(key, attribute[key]);
                 }
             }
-        })();
-    };
+            children.forEach(child => {
+                if (typeof child === 'string') {
+                    element.appendChild(document.createTextNode(child));
+                } else {
+                    element.appendChild(child);
+                }
+            });
+            return element;
+        }
+        return {
+            createElement: createElement
+        }
+    })();
+
+    const validationModule = (() => {
+        const isValidInput = (input) => {            
+            console.log("input:", input)
+
+            return input && input.length >= 1 && input.length <= 10 && /^[a-zA-Z0-9!@#$%^&*()_+,\-./:;<=>?@[\]^_`{|}~]+$/.test(input);
+        };
+        const validatePlayerName = (input) => {
+            if (domHelpers.getRadioValue() !== 'player2' || isValidInput(input)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return { validatePlayerName } 
+    })();
+
     
-    const updateScores = (winner, loser) => {
 
-    }
+    // Creates players displays, and shows their names and score
+    const renderModule = (() => {
+        const player1Name = domElements.mainElement.querySelector('#player1-name');
+        const player2Name = domElements.mainElement.querySelector('#player2-ai-name');
+        const player1Input = domElements.mainElement.querySelector('.get-player1-name-input');
+        const player2Input = domElements.mainElement.querySelector('.get-player2-name-input');
 
-    return {
-        renderPlayerInfo,
-        updateScores
-    }
+            // This will keep the player 2 name up-to-date
+        const updatePlayer2Name = () => {
+                let aiName = '';
+                switch (domHelpers.getRadioValue()) {
+                    case 'easy': aiName = "Locke"; break;
+                    case 'normal': aiName = "Tom"; break;
+                    case 'hard': aiName = "John"; break;
+                    default: aiName = "Name Here"; break;
+                }
+                player2Name.textContent = aiName;
+        }
+
+        const renderPlayerInfo = () => {
+            if (validationModule.validatePlayerName(domHelpers.player1NameInput()) === false || validationModule.validatePlayerName(domHelpers.player2NameInput()) === false) {
+                player1Input.placeholder = "REQUIRED";
+                player2Input.placeholder = "REQUIRED";
+            } else if (validationModule.validatePlayerName(domHelpers.player1NameInput()) === true || validationModule.validatePlayerName(domHelpers.player2NameInput()) === true) {
+                player1Name.textContent = domHelpers.player1NameInput();
+                if (domHelpers.getRadioValue() === 'player2') {
+                    player2Name.textContent = domHelpers.player2NameInput();
+                } 
+            } 
+        };
+        
+        const updateScores = (winner, loser) => {
+
+        }
+
+        return {
+            renderPlayerInfo,
+            updateScores,
+            updatePlayer2Name
+        }
+        
+        
+    })();
+
+    // This module will contain game information
+    const gameModule = (() => {
     
-    
-})();
+    })();
 
-// This module will contain game information
-const gameModule = (function() {
-   
-})();
+    // Event listener to show button
+    const listenerModule = (() => {
+        const listener = () => {
+            const newGameBtn = domElements.mainElement.querySelector('#new-game-btn');
+            newGameBtn.addEventListener('click', formControl.showForm);            
 
-// This is the module for the display
-const displayModule = (function(){
-    const displayBtn = domElements.mainElement.querySelector('.confirm-name-btn');
-        // Listen for the New Game Button to be pressed, and then display the board. 
-        displayBtn.addEventListener('click', (e) => {
-            e.preventDefault();
-            boardModule.createBoard();
-            formControl.hideForm();
-            renderModule.renderPlayerInfo();
-        });
+            const radioButtons = domElements.mainElement.querySelectorAll('[name="radio"]');
+            radioButtons.forEach(radio => {
+                radio.addEventListener('change', renderModule.updatePlayer2Name);
+            });
+        } 
+        return { listener }; 
+    })();
+     
+    // This is the module for the display
+    const displayModule = (function(){
+        const displayBtn = domElements.mainElement.querySelector('.confirm-name-btn');    
+            // Listen for the New Game Button to be pressed, and then display the board. 
+            const displayListener = (() => { 
+                displayBtn.addEventListener('click', (e) => {             
+                    e.preventDefault();
 
-})();
+                    if (validationModule.validatePlayerName(domHelpers.player1NameInput()) && 
+                        validationModule.validatePlayerName(domHelpers.player2NameInput())) {
+                            formControl.hideForm(); 
+                            boardModule.createBoard();
+                        } 
+                    renderModule.renderPlayerInfo();
+                });
+            })();    
+    })();
 
-listenerModule.listener();
+    listenerModule.listener();
+});
